ffb0a3bd151fdcc72887d3f87a22ec2c0bc80579
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-01-17 22:21:34 -0300

    🎨 Melhoria no design

--- obj/Debug/net8.0/EstudosApiFront.AssemblyInfo.cs
+++ obj/Debug/net8.0/EstudosApiFront.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstudosApiFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30677a3ab2e6e10680e8486bbd53469010c1c014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e3287ab115f84c95677191b8737779aa509e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstudosApiFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstudosApiFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
